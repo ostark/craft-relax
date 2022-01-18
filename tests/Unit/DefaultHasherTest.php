b5eb98c5916e5e116d387768933755217750a5be
Oliver Stark <os@fortrabbit.com>
2022-01-18 23:43:10 +0100

    Test for DefaultHasher

---
+++ tests/Unit/DefaultHasherTest.php
@@ -0,0 +1,30 @@
+<?php
+
+it('creates a hash from a job object with a time formatted suffix', function () {
+    $serializer = new \yii\queue\serializers\PhpSerializer();
+    $hasher = new \ostark\Relax\Queue\DefaultHasher($serializer);
+
+    $separator = '__';
+    $result = $hasher->hash(new DummyJob());
+    [$md5, $suffix] = explode($separator, $result, 2);
+
+    expect($md5)->toHaveLength(32);
+    expect($suffix)->toEndWith('00');
+});
+
+
+
+class DummyJob implements \craft\queue\JobInterface
+{
+    public int $prop = 1;
+
+    public function getDescription()
+    {
+        return 'dummy';
+    }
+
+    public function execute($queue)
+    {
+
+    }
+}
