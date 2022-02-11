7a923481e5f8cdda3fc386dfac08ba6bac372018
Oliver Stark <os@fortrabbit.com>
2022-02-11 11:47:37 +0100

    Test the job hash

--- tests/Unit/DefaultHasherTest.php
+++ tests/Unit/DefaultHasherTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 it('creates a hash from a job object with a time formatted suffix', function () {
     $serializer = new \yii\queue\serializers\PhpSerializer();
     $hasher = new \ostark\Relax\Queue\DefaultHasher($serializer);
@@ -8,11 +10,14 @@ it('creates a hash from a job object with a time formatted suffix', function () 
     $result = $hasher->hash(new DummyJob());
     [$md5, $suffix] = explode($separator, $result, 2);
 
+    // Not more than 64 chars to fit in the db column
     expect(mb_strlen($result))->toBeLessThanOrEqual(64);
+    expect(mb_strlen($result))->toBeGreaterThan(40);
+
+    // Expected format: {md5}__{Ymd.His}
     expect($md5)->toHaveLength(32);
     expect($suffix)->toEndWith('00');
 });
-
 
 
 class DummyJob implements \craft\queue\JobInterface
@@ -26,6 +31,5 @@ class DummyJob implements \craft\queue\JobInterface
 
     public function execute($queue)
     {
-
     }
 }
